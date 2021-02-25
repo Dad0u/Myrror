0926e02ba41264efe63a15a35d2ca2cdf1c8be49
Victor Couty <victor@couty.eu>
2021-02-25 22:56:08 +0100

    Added function to group files by property

--- myrror.py
+++ myrror.py
@@ -33,7 +33,6 @@ def get_match(src,dst,comparison):
   # [([src1,src2,...],[dst1,...]),([],[]),...]
 
 
-
 def same(attr):
   def f(a,b):
     return getattr(a,attr) == getattr(b,attr)
@@ -46,23 +45,58 @@ def mkloc(p):
   print(f"Found {len(loc.flist)} files on {loc}")
   return loc
 
+
 # size is implied
-default_check = [['name','path','size','mtime'],['qhash','parthash_10']]
+default_check = [['fullpath','size','mtime'],['qhash','parthash_10']]
 strict_check = [['qhash','sha256']]
 
-def sync_folders(src,dst):
+
+def group(l,attr):
+  """
+  Refine a grouping using the given attribute
+
+  Takes and returns a list of tuples of lists of files
+  [([src1,src2],[dst1]),([src3],[]),([],[dst3])]
+  in the tuple, both lists can hold any number of files
+  except 0 on both sides
+  """
+  r = []
+  for ls,ld in l:
+    ds,dd = {},{}
+    for f in ls:
+      k = getattr(f,attr)
+      if k in ds:
+        ds[k].append(f)
+      else:
+        ds[k] = [f]
+    for f in ld:
+      k = getattr(f,attr)
+      if k in dd:
+        dd[k].append(f)
+      else:
+        dd[k] = [f]
+    for k,v in ds.items():
+      r.append((v,dd.pop(k,[])))
+    for v in dd.values():
+      r.append(([],v))
+  return r
+
+
+def sync_folders(src,dst,check=default_check):
   #src,dst = Location(src),Location(dst)
   #src.discover_files()
   #dst.discover_files()
   with Pool(2) as p:
     src,dst = p.map(mkloc,[src,dst])
   # Those files are considered identical
-  # For each sequence in a check, keep groups of files that fullfill all the checks
+  # For each sequence in a check, keep groups of files
+  # that fullfill all the checks
   # Maybe group ALL the files for local matches...
-  # Store the partial and full hashes (and qhashes ? -> benchmark to see if useful or not)
-
-
-
+  # Store the partial and full hashes
+  # (and qhashes ? -> benchmark to see if useful or not)
+  for group in check:
+    for attr in group:
+      pass
 
 
 if __name__ == '__main__':
