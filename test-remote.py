a7cf623ca5c17c3600859ccda76b4d9622f7d3d9
Victor Couty <victor@couty.eu>
2021-04-05 22:41:56 +0200

    Updated test-remote script

--- test-remote.py
+++ test-remote.py
@@ -1,11 +1,7 @@
 import subprocess
 
-host = "localhost"
-path = "/home/vic/projects/config/"
-
-subprocess.run(f'rsync -avu myrror-server.py {host}:{path}',shell=True)
-subprocess.run(f'rsync -avu file.py {host}:{path}',shell=True)
-p = subprocess.run(f'ssh {host} "cd {path} && python3 myrror-server.py list {path}"',shell=True,capture_output=True,encoding='utf-8')
+host = "zeropi.home"
+path = "/home/vic/hdd/raid/Musique/Santiano"
 
 
 class F:
@@ -14,10 +10,45 @@ class F:
     self.size = size
     self.mtime = mtime
 
+  def __repr__(self):
+    return self.path
+
 
 def str_to_files(s):
-  l = s.split('\n')
+  lines = s.split('\n')
   r = []
-  for path,size,mtime in zip(l[::3],l[1::3],l[2::3]):
-    r.append(F(path,size,mtime))
+  current = 2
+  d = {"name":"#n>","size":"#s>","mtime":"#t>"}
+  dl = ['name','size','mtime']
+  s = {}
+  for line in lines:
+    current = (current + 1)%3
+    if not line.startswith(d[dl[current]]):
+      if line:
+        print("Ignored line:\n",line)
+      continue
+    else:
+      s[dl[current]] = line[3:]
+    if current == 2:
+      r.append(F(s['name'],s['size'],s['mtime']))
   return r
+
+
+p = subprocess.run(
+    f'ssh {host} python3 < myrror-server.py - list "{path}"',
+    shell=True,capture_output=True,encoding='utf-8')
+r = str_to_files(p.stdout)
+
+print(len(r))
+for f in r:
+  print(f)
+
+with open('test.txt','w') as fi:
+  fi.write(path+'\n')
+  for f in r:
+    fi.write(f.path+"\n")
+subprocess.run(f'scp -C test.txt {host}:{path}/.myrror-toupdate.txt',shell=True)
+p = subprocess.run(f'ssh {host} python3 < myrror-server.py - get_qhash {path}/.myrror-toupdate.txt',
+    shell=True,capture_output=True,encoding='utf-8')
+print(p.stdout)
+subprocess.run(f'ssh {host} rm {path}/.myrror-toupdate.txt',shell=True)
