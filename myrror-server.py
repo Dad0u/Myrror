22d4425392f8dff587e7510295411ad238f4d15d
Victor Couty <victor@couty.eu>
2021-06-09 17:08:46 +0200

    PEP8 formatting for myrror-server

--- myrror-server.py
+++ myrror-server.py
@@ -12,7 +12,7 @@ def get_all_files(d):
   for f in l:
     if f.startswith('.'):
       continue
-    full = os.path.join(absd,f)
+    full = os.path.join(absd, f)
     if os.path.isdir(full):
       r += get_all_files(full)
     else:
@@ -20,12 +20,12 @@ def get_all_files(d):
   return r
 
 
-def md5(fname,bs=1048576):
+def md5(fname, bs=1048576):
   """
   Returns the MD5 checksum of the file at the given location
   """
   h = hashlib.md5()
-  with open(fname,'rb') as f:
+  with open(fname, 'rb') as f:
     chunk = f.read(bs)
     while chunk:
       h.update(chunk)
@@ -33,12 +33,12 @@ def md5(fname,bs=1048576):
     return h.hexdigest()
 
 
-def sha256(fname,bs=1048576):
+def sha256(fname, bs=1048576):
   """
   Returns the SHA256 checksum of the file at the given location
   """
   h = hashlib.sha256()
-  with open(fname,'rb') as f:
+  with open(fname, 'rb') as f:
     chunk = f.read(bs)
     while chunk:
       h.update(chunk)
@@ -46,7 +46,7 @@ def sha256(fname,bs=1048576):
     return h.hexdigest()
 
 
-def quick_hash_file(fname,bs=1024):
+def quick_hash_file(fname, bs=1024):
   """
   Returns a quicker hash of the file at the given location
 
@@ -54,52 +54,52 @@ def quick_hash_file(fname,bs=1024):
   Warning! Changing the bs will change the value of the hash
   """
   size = os.path.getsize(fname)
-  if size <= 4*bs:
-    return md5(fname,bs)
+  if size <= 4 * bs:
+    return md5(fname, bs)
   h = hashlib.md5()
-  with open(fname,'rb') as f:
+  with open(fname, 'rb') as f:
     h.update(f.read(bs))
-    f.seek(size//2,0)
+    f.seek(size // 2, 0)
     h.update(f.read(bs))
-    f.seek(-bs,2)
+    f.seek(-bs, 2)
     h.update(f.read(bs))
   return h.hexdigest()
 
 
-def partial_hash(f,percent=10,bs=1048576):
+def partial_hash(f, percent=10, bs=1048576):
   """
   Reads at least percent % of the file to make the hash
   """
   assert 0 < percent < 100
   size = os.path.getsize(f)
-  if size <= 3*bs:
+  if size <= 3 * bs:
     return md5(f)
   h = hashlib.md5()
-  nblocks = max(1,int(percent/100*size/bs/3))
-  with open(f,'rb') as f:
+  nblocks = max(1, int(percent / 100 * size / bs / 3))
+  with open(f, 'rb') as f:
     for i in range(nblocks):
       h.update(f.read(bs))
-    f.seek(int((size*(.5-percent/600))),0)
+    f.seek(int((size * (.5 - percent / 600))), 0)
     for i in range(nblocks):
       h.update(f.read(bs))
-    f.seek(-bs*nblocks,2)
+    f.seek(-bs * nblocks, 2)
     for i in range(nblocks):
       h.update(f.read(bs))
   return h.hexdigest()
 
 
 properties = {
-    'md5':md5,
-    'sha256':sha256,
-    'qhash':quick_hash_file,
-    'phash':partial_hash,
-  }
+    'md5': md5,
+    'sha256': sha256,
+    'qhash': quick_hash_file,
+    'phash': partial_hash,
+}
 
 
 if __name__ == '__main__':
   import sys
-  SAVE_DELAY = 60 # When reading properties, save them every X seconds
-  assert len(sys.argv) == 3,"Invalid command"
+  SAVE_DELAY = 60  # When reading properties, save them every X seconds
+  assert len(sys.argv) == 3, "Invalid command"
   if sys.argv[1] == "list":
     root = sys.argv[2]
     for fullpath in get_all_files(root):
@@ -110,22 +110,23 @@ if __name__ == '__main__':
     import pickle
     from time import time
     # Get the % of phash if necessary
-    prop,*index = sys.argv[1][4:].split('_')
-    assert len(index) in (0,1),"Unknown command: "+sys.argv[1]
+    prop, *index = sys.argv[1][4:].split('_')
+    assert len(index) in (0, 1), "Unknown command: " + sys.argv[1]
     if index:
       index = int(index[0])
       assert 0 < index < 100
     else:
       index = None
-    assert prop in properties,"Unknown arg: "+prop
+    assert prop in properties, "Unknown arg: " + prop
     lfile = sys.argv[2]
 
     long_prop = prop if not index else f'{prop}_{index}'
     # Managing the saved properties: try to open the files
-    with open(lfile,'r') as f:
-      root,*flist = [l[:-1] for l in f.readlines()]
+    with open(lfile, 'r') as f:
+      root, *flist = [l[:-1] for l in f.readlines()]
     try:
-      with open(os.path.join(root,f'.myrror-cached-{long_prop}.p'),'rb') as sf:
+      with open(os.path.join(
+           root, f'.myrror-cached-{long_prop}.p'), 'rb') as sf:
         saved = pickle.load(sf)
     except FileNotFoundError:
       saved = {}
@@ -134,26 +135,26 @@ if __name__ == '__main__':
     # Main loop: checking if the attr exists, compute it if necessary
     # save the file every SAVE_DELAY second and prints it
     for f in flist:
-      af = os.path.join(root,f)
+      af = os.path.join(root, f)
       if f in saved:
-        oldsize,oldmtime,oldr = saved[f]
-        size,mtime = os.path.getsize(af),os.path.getmtime(af)
-        if (oldsize,oldmtime) == (size,mtime):
+        oldsize, oldmtime, oldr = saved[f]
+        size, mtime = os.path.getsize(af), os.path.getmtime(af)
+        if (oldsize, oldmtime) == (size, mtime):
           r = oldr
         else:
           r = properties[prop](af)
-          saved[f] = (size,mtime,r)
+          saved[f] = (size, mtime, r)
       else:
         r = properties[prop](af)
-        saved[f] = (os.path.getsize(af),os.path.getmtime(af),r)
+        saved[f] = (os.path.getsize(af), os.path.getmtime(af), r)
       t1 = time()
       if t1 - t0 > SAVE_DELAY:
         with open(os.path.join(root,
-          f'.myrror-cached-{long_prop}.p'),'w') as sf:
-          pickle.dump(saved,sf)
+                               f'.myrror-cached-{long_prop}.p'), 'w') as sf:
+          pickle.dump(saved, sf)
         t0 = t1
       print(f'#p>{r}')
-    with open(os.path.join(root, f'.myrror-cached-{long_prop}.p'),'wb') as sf:
-      pickle.dump(saved,sf)
+    with open(os.path.join(root, f'.myrror-cached-{long_prop}.p'), 'wb') as sf:
+      pickle.dump(saved, sf)
   else:
-    raise NameError("Unknown command: "+sys.argv[1])
+    raise NameError("Unknown command: " + sys.argv[1])
