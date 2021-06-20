e1593457df239975953445e991f954fbf7361fb2
Victor Couty <victor@couty.eu>
2021-06-20 17:42:15 +0200

    Started grouping method in compare.py

--- compare.py
+++ compare.py
@@ -53,7 +53,26 @@ def refine(groups: list, key: str) -> list:
   return r
 
 
-def compare(src: Location, dst: Location) -> dict:
+def remove_uniques(groups: list) -> list:
+  return [i for i in groups if len(i) == 1], [i for i in groups if len(i) != 1]
+
+
+def compare(src: Location, dst: Location, criteria: list) -> dict:
+  """
+  To compare two locations based on a list of criteria
+
+  Takes two locations and a list of criteria to match the files
+  Returns a list of list of Files. Each list is a matching group
+  Each file has a 'location' key either set to 'src' or 'dst'
+  The list of criteria can be a list of keys or a list of lists of keys
+  In the first case, all the keys must match, in the second, all the keys of
+  at least one list must match. It can be used to accelerate comparison in the
+  where a set of attribute is less likely to match easier to compute and still
+  reliable (example: in some cases, name, path and mtime matching is enough to
+  exclude a content comparison)
+  """
+  if isinstance(criteria[0], str):
+    criteria = [criteria]
   print("Enumerating files...", end='', flush=True)
   with Pool(2) as p:
     src_list, dst_list = p.map(build_file_list, (src, dst))
@@ -63,3 +82,8 @@ def compare(src: Location, dst: Location) -> dict:
   for f in dst_list:
     f['location'] = 'dst'
   groups = refine([src_list + dst_list], 'size')
+  unique, nonunique = remove_uniques(groups)
+
+  for cgp in criteria:
+    for criterion in cgp:
+
