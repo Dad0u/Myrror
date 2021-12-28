09699a21653d670111b1c59fe69f83cb3d09a2a0
Victor Couty <victor@couty.eu>
2021-12-28 23:05:57 +0100

    [add] core.py, a higher level for files and locations

---
+++ core.py
@@ -0,0 +1,31 @@
+from file import File, Location
+import remote
+import local
+
+
+def listf(loc: Location, exclude=tuple()):
+  """
+  Returns a list of File objects in the Location
+
+  Works with either local or remote Locations
+  """
+  if loc.is_local:
+    return local.listf(loc, exclude)
+  # else:
+  return remote.listf(loc, exclude)
+
+
+def get(prop: str, flist: list[File]):
+  """
+  Get a property in a list of files
+  """
+  s = set()
+  for f in flist:
+    s.add(f.loc)
+  if len(s) != 1:
+    raise AttributeError("Can only call get() on a list of files "
+        "from the same location")
+  if s.pop().is_local:
+    return local.get(prop, flist)
+  # else:
+  return remote.get(prop, flist)
