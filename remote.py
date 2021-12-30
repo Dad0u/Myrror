ea0ba3243f9f4888f9d4d2057a7872810c0ee86b
Victor Couty <victor@couty.eu>
2021-12-30 23:07:43 +0100

    [fix] Two major errors in remote.py (how did that even work??)

--- remote.py
+++ remote.py
@@ -9,18 +9,18 @@ TMP = '.myrror-tmp.txt'
 def str_to_files(s):
   lines = s.split('\n')
   r = []
-  current = 2
+  current = 0
   d = {"path": "#n>", "size": "#s>", "mtime": "#t>"}
   dl = ['path', 'size', 'mtime']
   s = {}
   for line in lines:
-    current = (current + 1) % 3
     if not line.startswith(d[dl[current]]):
       if line:
-        print("Ignored line:\n", line)
+        print("Ignored line:\n" + line)
       continue
     s[dl[current]] = line[3:]
-    if current == 2:
+    current = (current + 1) % 3
+    if current == 0:
       r.append(dict(s))
   return r
 
@@ -75,16 +75,16 @@ def get(prop, flist):
       f'scp -C {TMP} {loc.host}:{loc.directory}/.myrror-toupdate.txt',
       shell=True, check=True)
   os.remove(TMP)
-  p = subprocess.run(f'ssh {loc.host} python3 < myrror-server.py - "\
-      "get_{prop} {loc.directory}/.myrror-toupdate.txt',
-       shell=True, capture_output=True, encoding='utf-8', check=True)
+  p = subprocess.run(f'ssh {loc.host} python3 < myrror-server.py - '
+                     f'get_{prop} "{loc.directory}/.myrror-toupdate.txt"',
+                     shell=True, capture_output=True, encoding='utf-8',
+                     check=True)
   subprocess.run(f'ssh {loc.host} rm {loc.directory}/.myrror-toupdate.txt',
-      shell=True, check=True)
+                 shell=True, check=True)
   r = str_to_prop(p.stdout)
   assert len(r) == len(flist), "Failed getting properties of the files!"
   for f, p in zip(flist, r):
     setattr(f, prop, p)
-  return flist
 
 
 if __name__ == '__main__':
